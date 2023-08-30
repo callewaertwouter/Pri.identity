9d83e5c9c4574d7a1c32efe16231b6517efeb28a
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-30 22:01:48 +0200

    Add token generation

---
+++ Pri.identity.Api/Dtos/Accounts/LoginUserResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Pri.identity.Api.Dtos.Accounts
+{
+	public class LoginUserResponseDto
+	{
+        public string Token { get; set; }
+    }
+}
--- Pri.identity.Api/Program.cs
+++ Pri.identity.Api/Program.cs
@@ -1,7 +1,41 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Pri.identity.Api.Data;
+using Pri.identity.Api.Entities;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+
+// Identity configuration
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => // AddIdentity because UI package is not needed, if UI is needed then AddDefaultIdentity<ApplicationUser>
+	{ 
+		options.SignIn.RequireConfirmedEmail = false; 
+	})
+	.AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(option => 
+{ 
+	option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+	option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+})
+.AddJwtBearer(jwtOptions => 
+{ 
+	jwtOptions.TokenValidationParameters = new TokenValidationParameters() 
+	{ 
+		ValidateActor = true, 
+		ValidateAudience = true, 
+		ValidateLifetime = true, 
+		ValidIssuer = builder.Configuration["JWTConfiguration:Issuer"], 
+		ValidAudience = builder.Configuration["JWTConfiguration:Audience"], 
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTConfiguration:SigningKey"])) 
+	}; 
+});
 
 var app = builder.Build();
 
@@ -18,6 +52,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
